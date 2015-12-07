4ff442747f53863868c12df947d40478f3bd65f6
GraninNikita <ya.cpec@gmail.com>
2015-12-07 03:05:26 +0300

    added google map

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -338,8 +338,13 @@
             <div class="col-md-offset-2 col-md-8 text-center bigger-text">
             </div>
             <div class="col-md-6 col-sm-6">
-                <div id="map">
-                </div>
+                {{--<div id="map">--}}
+                {{--</div>--}}
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5639.490346441263!2d35.38147678465577!3d45.03009821927149!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40eb85f385944043%3A0x4060a0ecd2dbe50e!2z0YHRgtCw0L3RhtC40Y8g0KTQtdC-0LTQvtGB0LjRjw!5e0!3m2!1sru!2s!4v1449446555861" width="555" height="380" frameborder="0" style="border:0" allowfullscreen></iframe>
+                {{-- Карта города Феодосия--}}
+                {{--<script type="text/javascript" charset="utf-8"--}}
+                        {{--src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=OAau7AYtMs54Sg4dGUhEsCnihgSqvFeM&width=500&height=400&lang=ru_RU&sourceType=constructor">--}}
+                {{--</script>--}}
             </div> <!-- /.col-md-6 -->
             <div class="col-md-6 col-sm-6">
 
